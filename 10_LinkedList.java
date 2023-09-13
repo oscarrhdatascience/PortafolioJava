6a947a1d309fe51e97729a6db8d2d25082b6f696
Oscar Rodriguez Hernandez <91960921+oscarrhdatascience@users.noreply.github.com>
2023-09-13 10:46:56 +0200

    Create 10_LinkedList.java

---
+++ 10_LinkedList.java
@@ -0,0 +1,42 @@
+public class LinkedList {
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node lastNode = head;
+        while (lastNode.next != null) {
+            lastNode = lastNode.next;
+        }
+        lastNode.next = newNode;
+    }
+
+    public void printList() {
+        Node currentNode = head;
+        while (currentNode != null) {
+            System.out.println(currentNode.data);
+            currentNode = currentNode.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.append(1);
+        ll.append(2);
+        ll.append(3);
+        ll.printList();  // 1 2 3
+    }
+}
